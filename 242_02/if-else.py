b36fdd1b7ad7abf56e4563823807b785eb5b78cb
Jit Sinha <131513604+justintime2023@users.noreply.github.com>
2024-07-05 13:11:22 +0530

    Add files via upload

---
+++ 242_02/if-else.py
@@ -0,0 +1,9 @@
+x = 0
+if x > 0:
+    print("x is positive") 
+    if x >= 10: 
+        print("x is also greater than or equal to 10") 
+    else: 
+        print("x is positive but less than 10") 
+else: 
+    print("x is non-positive") 
---
+++ 242_02/students.py
@@ -0,0 +1,8 @@
+# Total number of students
+students = 23 
+# Determine groups and remaining students
+groups = students // 4
+remaining_students = students % 4
+# Print the results
+print(f"Number of groups: {groups}") 
+print(f"Students without a group: {remaining_students}") 
