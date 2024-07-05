8223afd558de953bf87326f4b65b2af45575fb84
Jit Sinha <131513604+justintime2023@users.noreply.github.com>
2024-07-05 12:39:30 +0530

    Add files via upload

---
+++ 242_03/Understanding While Loops Through a Countdown Timer Example.py
@@ -0,0 +1,10 @@
+# Initiation: Set the starting point of the countdown 
+counter = 5 
+# While keyword initiates the loop 
+while counter > 0: # The condition to be checked before each loop iteration 
+    # Execution: The code block that executes as long as the condition is true 
+    print(counter) # This line prints the current value of 'counter' 
+    # Progression: Decrement the counter to eventually meet the loop termination condition 
+    counter -= 1 
+# Conclusion: The loop ends when the condition is false (counter is no longer greater than 0) 
+print("Countdown finished!") 
