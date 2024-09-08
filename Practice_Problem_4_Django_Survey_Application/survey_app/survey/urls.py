ec969b1765827733ddd8db072c9ee2a7ceda6c1c
Jit Sinha <131513604+justintime2023@users.noreply.github.com>
2024-09-08 12:48:01 +0530

    Delete Practice_Problem_4_Django_Survey_Application directory

--- Practice_Problem_4_Django_Survey_Application/survey_app/survey/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.survey_list, name='survey_list'),
-    path('<int:survey_id>/', views.survey_detail, name='survey_detail'),
-    path('<int:survey_id>/results/', views.survey_results, name='survey_results'),
-]
