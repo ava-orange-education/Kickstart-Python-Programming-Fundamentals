1e2a1faf025a40e4dee197c2221663d413682548
Jit Sinha <131513604+justintime2023@users.noreply.github.com>
2024-09-08 12:02:24 +0530

    Delete 242_06 directory

--- 242_06/Understanding Constructors.py
+++
@@ -1,5 +0,0 @@
-class Book: 
-    def __init__(self, title, author): 
-        self.title = title # Instance attribute for the book's title 
-        self.author = author # Instance attribute for the book's author 
-my_book = Book("1984", "George Orwell") 
--- 242_06/understanding syntax of class.py
+++
@@ -1,9 +0,0 @@
-class Animal: 
-    # Class attribute 
-    is_living = True 
-    # Method defined within the class 
-    def speak(self): 
-        return "This animal makes a sound" 
-generic_animal = Animal() 
-print(generic_animal.is_living) # Outputs: True 
-print(generic_animal.speak()) # Outputs: This animal makes a sound 
