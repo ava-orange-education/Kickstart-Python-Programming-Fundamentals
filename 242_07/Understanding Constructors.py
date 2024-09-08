74a2c7fdf88fbad6d05551a4d8dbe3ede830fc21
Jit Sinha <131513604+justintime2023@users.noreply.github.com>
2024-09-08 12:10:05 +0530

    Add files via upload

---
+++ 242_07/Understanding Constructors.py
@@ -0,0 +1,5 @@
+class Book: 
+    def __init__(self, title, author): 
+        self.title = title # Instance attribute for the book's title 
+        self.author = author # Instance attribute for the book's author 
+my_book = Book("1984", "George Orwell") 
