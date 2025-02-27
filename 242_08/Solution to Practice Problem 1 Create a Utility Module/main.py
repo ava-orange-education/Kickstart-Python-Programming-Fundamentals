bdc93a877b90a73a94b079acf355e41be5a0340a
Jit Sinha <131513604+justintime2023@users.noreply.github.com>
2025-02-28 02:57:35 +0530

    Update main.py

--- 242_08/Solution to Practice Problem 1 Create a Utility Module/main.py
+++ 242_08/Solution to Practice Problem 1 Create a Utility Module/main.py
@@ -1,9 +1,9 @@
 # Import the custom utilities module 
-import utilities 
+from utilities import celsius_to_fahrenheit, fahrenheit_to_celsius
 # Test the temperature conversion functions 
 # Convert 100 degrees Fahrenheit to Celsius 
-temp_in_celsius = utilities.fahrenheit_to_celsius(100) 
+temp_in_celsius = fahrenheit_to_celsius(100) 
 print(f"100 degrees Fahrenheit is {temp_in_celsius:.2f} degrees Celsius.") 
 # Convert 37 degrees Celsius to Fahrenheit 
-temp_in_fahrenheit = utilities.celsius_to_fahrenheit(37) 
+temp_in_fahrenheit = celsius_to_fahrenheit(37) 
 print(f"37 degrees Celsius is {temp_in_fahrenheit:.2f} degrees Fahrenheit.")
