b7e8e3b5ed5104dc18e5c5a5731e6fc4134b0ec2
Jit Sinha <131513604+justintime2023@users.noreply.github.com>
2025-03-04 23:06:29 +0530

    Add files via upload

--- 242_10/Reading from Files.py
+++ 242_10/Reading from Files.py
@@ -1,15 +1,15 @@
 # Using the 'with' statement for better resource management
-with open("example.txt", "r") as file:
+with open("./data/example.txt", "r") as file:
     content = file.read()
     print(content)
 
-with open("example.txt", "r") as file:
+with open("./data/example.txt", "r") as file:
     line = file.readline()
     while line:
         print(line.strip())  # strip() removes the newline character
         line = file.readline()
         
-with open("example.txt", "r") as file:
+with open("./data/example.txt", "r") as file:
     lines = file.readlines()
     for line in lines:
         print(line.strip())  # strip() removes the newline character
--- 242_10/Using open() and close() Functions.py
+++ 242_10/Using open() and close() Functions.py
@@ -1,5 +1,5 @@
 # Opening a file
-file = open("example.txt", "r")  # 'r' mode opens the file for reading
+file = open("./data/example.txt", "r")  # 'r' mode opens the file for reading
 
 # Performing operations (e.g., reading the file content)
 content = file.read()
